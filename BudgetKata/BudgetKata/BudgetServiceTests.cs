40d907a995fa965b7f76cf23c7659804042d0fcf
erwin <erwin.chang@titansoft.com.sg>
2022-10-24 16:31:19 +0800

    no budget test

---
+++ BudgetKata/BudgetKata/BudgetServiceTests.cs
@@ -0,0 +1,37 @@
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BudgetKata;
+
+public class BudgetServiceTests
+{
+    private BudgetService _budgetService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _budgetService = new BudgetService();
+    }
+
+    [Test]
+    public void no_budget()
+    {
+        var totalAmount = WhenQuery(new DateTime(2022, 10, 1), new DateTime(2022, 10, 31));
+        totalAmount.Should()!.Be(0m);
+    }
+
+    private decimal WhenQuery(DateTime startDate, DateTime endDate)
+    {
+        var query = _budgetService.Query(startDate, endDate);
+        return query;
+    }
+}
+
+public class BudgetService
+{
+    public decimal Query(DateTime startDate, DateTime endDate)
+    {
+        return 0m;
+    }
+}
