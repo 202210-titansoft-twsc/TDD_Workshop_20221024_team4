3a7b5e902cde58227985a42ec86f7bebd315d8b6
erwin <erwin.chang@titansoft.com.sg>
2022-10-24 17:35:04 +0800

    refactor: remove unuse code and change map signature

--- BudgetKata/BudgetKata/BudgetService.cs
+++ BudgetKata/BudgetKata/BudgetService.cs
@@ -19,34 +19,28 @@ public class BudgetService
     public decimal Query(DateTime startDate, DateTime endDate)
     {
         var budgets = _budgeRepo.GetAll();
-        var daysInMonth = DateTime.DaysInMonth(startDate.Year, startDate.Month);
 
-        
         if (budgets.Any())
         {
             var result = 0m;
-            var dailyAmounts = new Dictionary<DateTime, decimal>();
+            var dailyAmounts = new Dictionary<string, decimal>();
 
             foreach (var budget in budgets)
             {
                 var date = DateTime.ParseExact(budget.YearMonth, "yyyyMM", CultureInfo.InvariantCulture);
                 var daysInThisMonth = DateTime.DaysInMonth(date.Year, date.Month);
                 var dailyAmount = (decimal)budget.Amount / daysInThisMonth;
-                dailyAmounts.Add(date, dailyAmount);
+                dailyAmounts.Add(budget.YearMonth, dailyAmount);
             }
 
             var currentDate = startDate;
             while (currentDate <= endDate)
             {
-
-                result+= dailyAmounts[new DateTime(currentDate.Year, currentDate.Month, 1)];
+                result+= dailyAmounts[currentDate.ToString("yyyyMM")];
                 currentDate = currentDate.AddDays(1);
             }
 
             return result;
-            var totalDays = (decimal)(endDate - startDate).TotalDays + 1;
-
-            return (decimal)budgets.First().Amount / daysInMonth * totalDays;
         }
 
         return 0;
