3c6adfeb4ee6488a7442683bee5d449e40b65285
erwin <erwin.chang@titansoft.com.sg>
2022-10-24 17:08:21 +0800

    refactor: use totalDays to calculate

--- BudgetKata/BudgetKata/BudgetService.cs
+++ BudgetKata/BudgetKata/BudgetService.cs
@@ -17,22 +17,13 @@ public class BudgetService
         var budgets = _budgeRepo.GetAll();
         var daysInMonth = DateTime.DaysInMonth(startDate.Year, startDate.Month);
 
-        
         if (budgets.Any())
         {
             var totalDays = (decimal)(endDate - startDate).TotalDays + 1;
 
-            if (totalDays >= 2)
-            {
-                return (decimal)budgets.First().Amount / daysInMonth * totalDays;
-            }
-
-            if (startDate == endDate)
-            {
-                return (decimal)budgets.First().Amount / daysInMonth;
-            }
-            return budgets.First().Amount;
+            return (decimal)budgets.First().Amount / daysInMonth * totalDays;
         }
+
         return 0;
     }
 }
